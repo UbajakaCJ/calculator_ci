5a54bb23d90d9edbf854fb48e0102613c8b698b7
Chijioke Ubajaka <ubajaka.chijioke@gmail.com>
2023-11-13 10:48:07 +0100

    1st commit

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,23 @@ Unit tests for the calculator library
 
 import calculator
 
-class TestCalculator:
+# class TestCalculator:
 
+#     def test_addition(self):
+#         assert 4 == calculator.add(2, 2)
+
+#     def test_subtraction(self):
+#         assert 2 == calculator.subtract(4, 2)
+
+
+import unittest
+
+class MyTest(unittest.TestCase):
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
-
+        self.assertEqual(calculator.add(2, 2), 4)
+    
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        self.assertEqual(calculator.subtract(5, 3), 2)
+
+if __name__ == '__main__':
+    unittest.main()
